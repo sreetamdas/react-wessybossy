3b84900ce9c66a827e545a389474c8109661cda3
Sreetam Das <sreetamdas@gmail.com>
2017-11-24 02:20:03 +0530

    Getting that form value

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    goToStore (event) {
+        event.preventDefault();
+        console.log("Hey Hey Hey!");
+        console.log(this.storeInput.value);
+    }
+
     render () {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)} >
                 <h2>Please enter a store</h2>
-                <input type="text" placeholder="Store Name" required />
+                <input type="text" placeholder="Store Name" required defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
                 <button type="submit">Visit Store -></button>
             </form>
         )
